9b255a811f1115cc2e0bbb7797d3678ac0e62bcf
Max <magicmaxmark@gmail.com>
2019-03-06 19:51:46 -0500

    I broke it in a different way

--- MaxTestBad 2 copy/Assets/CameraScrpit2.cs
+++ MaxTestBad 2 copy/Assets/CameraScrpit2.cs
@@ -9,13 +9,17 @@ public class CameraScrpit2 : MonoBehaviour
     public float mouseSensitivity = 100.0f;
     public float rotY;
     public float rotX;
-    public float rotZ;
+    public float rotZ = 0;
+    float currentX;
+    float currentY;
     public GameObject airplane;
     // Start is called before the first frame update
     void Start()
     {
         rotY = this.gameObject.transform.localRotation.y;
         rotX = this.gameObject.transform.localRotation.x;
+        currentX = ((airplane.transform.rotation.x + rotX * Time.deltaTime) - 0) * (180 - 0) / (1 - 0);
+        currentY = ((airplane.transform.rotation.y + rotY * Time.deltaTime) - 0) * (180 - 0) / (1 - 0);
     }
 
     // Update is called once per frame
@@ -26,14 +30,27 @@ public class CameraScrpit2 : MonoBehaviour
 
         rotY += mouseX * mouseSensitivity * Time.deltaTime;
         rotX += mouseY * mouseSensitivity * Time.deltaTime;
-        rotZ = 0.00f;
+
+        if (Input.GetKey(KeyCode.Q))
+        {
+            rotZ += 1 * Time.deltaTime;
+        }
+
+        if (Input.GetKey(KeyCode.E))
+        {
+            rotZ -= 1 * Time.deltaTime;
+        }
+
 
         rotY = Mathf.Clamp(rotY, -30, 30);
         rotX = Mathf.Clamp(rotX, -30, 30);
 
-        Quaternion localRotation = Quaternion.Euler(rotX, rotY, 0.0f);
+        Quaternion localRotation = Quaternion.Euler(rotX, rotY, rotZ);
         transform.localRotation = localRotation;
 
-        airplane.transform.localRotation *= Quaternion.Euler(rotX*Time.deltaTime, rotY*Time.deltaTime, rotZ*Time.deltaTime);
+        airplane.transform.rotation = Quaternion.Euler(currentX, currentY, 0);
+
+        currentX = ((airplane.transform.rotation.x + rotX * Time.deltaTime) - 0) * (180 - 0) / (1 - 0);
+        currentY = ((airplane.transform.rotation.y + rotY * Time.deltaTime) - 0) * (180 - 0) / (1 - 0);
     }
 }
